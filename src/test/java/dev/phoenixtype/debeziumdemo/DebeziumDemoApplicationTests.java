5f6618756c41a1af4a54266998c777f8725f7977
Samuel Akuma <usakuma@uwaterloo.ca>
2023-07-06 20:38:53 -0400

    Initial commit

---
+++ src/test/java/dev/phoenixtype/debeziumdemo/DebeziumDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phoenixtype.debeziumdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
